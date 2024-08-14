84f21f2ed9caef7dccbc589b5096d46fcf950849
2scent <ad7549@gmail.com>
2024-08-14 21:16:04 +0900

    feat: useSangtae가 상태만 반환하도록 수정

--- src/use-sangtae.ts
+++ src/use-sangtae.ts
@@ -1,16 +1,12 @@
 import { useSyncExternalStore } from 'react';
-import type { Sangtae } from 'tiny-sangtae';
+import type { Computed, Sangtae } from 'tiny-sangtae';
 
 /**
- * A React hook to use Sangtae state management.
+ * A React hook that integrates `Sangtae` or `Computed` with React's state management.
  *
- * @template State The type of the state managed by Sangtae.
- * @param {Sangtae<State>} sangtae The Sangtae instance to be used.
- * @returns {readonly [State, (updater: State | ((prev: State) => State)) => void]}
- * A tuple containing the current state and a function to update the state.
+ * @param sangtae The `Sangtae` or `Computed` state object to be used.
+ * @returns The current state value.
  */
-export function useSangtae<State>(sangtae: Sangtae<State>) {
-  const state = useSyncExternalStore(sangtae.subscribe, sangtae.get, sangtae.get);
-
-  return [state, sangtae.set] as const;
+export function useSangtae<State>(sangtae: Sangtae<State> | Computed<State>) {
+  return useSyncExternalStore(sangtae.subscribe, sangtae.get, sangtae.get);
 }
